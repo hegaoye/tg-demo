1bbb5dc19e642cd0af0b9ecf6d1391a9bde6830d
watson <watson@cg.support>
2022-07-20 19:47:49 +0800

    增加定时调度模式

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from tgbot.service.bot_service import BotService
 if __name__ == '__main__':
     logging.info("启动程序>>>")
     scheduler = AsyncIOScheduler(timezone='Asia/Shanghai')
-    scheduler.add_job(func=TableJob().run, trigger="interval", seconds=180)
+    scheduler.add_job(func=TableJob().run, trigger="interval", seconds=120)
     scheduler.start()
 
     bot_service = BotService()
