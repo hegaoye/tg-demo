008258c0d02dc27e66f397ff3264212d4e81888d
watson <watson@cg.support>
2022-07-20 12:16:49 +0800

    init

--- demo4.py
+++ demo4.py
@@ -1,6 +1,6 @@
 import sys
 
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update, CallbackQuery
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update, CallbackQuery, CallbackGame
 # creating updater
 from telegram.ext import Application, CallbackContext, CommandHandler, CallbackQueryHandler
 
@@ -19,37 +19,45 @@ async def start(update: Update, context: CallbackContext):
 
     # creating list of input buttons
     # documentation: https://python-telegram-bot.readthedocs.io/en/stable/telegram.inlinekeyboardbutton.html
-    keyboard = [[
-        InlineKeyboardButton("充值", callback_data='充值', url="https://www.google.com"),
-        InlineKeyboardButton("打折", callback_data='打折',
-                             web_app="https://medium.com/@algorithmassasin/build-an-e-commerce-telegram-web-app-bot-f96459e72f79")
-    ], [InlineKeyboardButton("小程序", callback_data='小程序')]]
+    demo_url = "https://www.wangcai321.com/bot/web/player.html?g=-1001756231525&t=YAyEYzQlNZ2w2Fo1658210341868#tgShareScoreUrl=tgb%3A%2F%2Fshare_game_score%3Fhash%3DVQfENQsIeEniRnaKwcQE"
+    g = CallbackGame()
+    g.set_bot(context.bot)
+    keyboard = [
+        [
+            InlineKeyboardButton(text="小程序", callback_game=g)
+            # InlineKeyboardButton(text="小程序", web_app=WebAppInfo(url=demo_url))
+            # InlineKeyboardButton(text="小程序", web_app=WebAppInfo(url="https://webapp.1akzl9mvs.com/"))
+            # InlineKeyboardButton(text="小程序", web_app=WebAppInfo(url="https://python-telegram-bot.org/static/webappbot"))
+        ]
+
+    ]
 
     # creating a reply markup of inline keyboard options
     # documentation: https://python-telegram-bot.readthedocs.io/en/stable/telegram.inlinekeyboardmarkup.html
     reply_markup = InlineKeyboardMarkup(keyboard)
-
     # sending the message to the current chat id
     # documentation: https://python-telegram-bot.readthedocs.io/en/stable/telegram.message.html#telegram.Message.reply_text
-    await update.message.reply_text('Please choose:', reply_markup=reply_markup)
+    await update.message.reply_game(game_short_name="thisiswatsonsdemogame")
+    # await update.message.reply_game(game_short_name="thisiswatsonsdemogame", reply_markup=reply_markup)
+    # await update.message.reply_text('Please choose:', reply_markup=reply_markup)
 
 
-async def button(update, context):
+async def button(update, context: CallbackContext):
     """
     callback method handling button press
     """
     # getting the callback query
     # documentation: https://python-telegram-bot.readthedocs.io/en/stable/telegram.callbackquery.html
     query: CallbackQuery = update.callback_query
-
     # CallbackQueries need to be answered, even if no notification to the user is needed
     # Some clients may have trouble otherwise. See https://core.telegram.org/bots/api#callbackquery
     # documentation: https://python-telegram-bot.readthedocs.io/en/stable/telegram.callbackquery.html#telegram.CallbackQuery.answer
-    query.answer()
+    demo_url = "https://www.wangcai321.com/bot/web/player.html?g=-1001756231525&t=YAyEYzQlNZ2w2Fo1658210341868#tgShareScoreUrl=tgb%3A%2F%2Fshare_game_score%3Fhash%3DVQfENQsIeEniRnaKwcQE"
 
+    await query.answer(text="open", url=demo_url)
     # editing message sent by the bot
     # documentation: https://python-telegram-bot.readthedocs.io/en/stable/telegram.callbackquery.html#telegram.CallbackQuery.edit_message_text
-    await query.edit_message_text(text="您选择了: {}".format(query.data))
+    # await query.edit_message_text(text="您选择了: {}".format("小程序"))
 
 
 # adding listeners
