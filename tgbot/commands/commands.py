d55a06cbde6d4a003c8e0b5042ff0c2548442126
watson <watson@cg.support>
2022-07-23 20:46:22 +0800

    完善投注订单

--- tgbot/commands/commands.py
+++ tgbot/commands/commands.py
@@ -57,7 +57,7 @@ class Command(Enum):
         elif text.__contains__(Command.NUM.value):
             bet_num = text_arr[1]
             bet_money = text_arr[2]
-            return Command.EVEN.name, bet_money, bet_num
+            return Command.NUM.name, bet_money, bet_num
 
     def custom_bet(self, text):
         """
