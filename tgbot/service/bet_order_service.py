741af7876c4e90b7811732b5a132580e99193bc8
watson <watson@cg.support>
2022-07-23 17:25:08 +0800

    實現按鍵投注額度的拉取

--- tgbot/service/bet_order_service.py
+++ tgbot/service/bet_order_service.py
@@ -1,18 +1,24 @@
+from tgbot.api.bet_api_client import BetApiClient
 from tgbot.api.bet_order_api_client import BetOrderApiClient
 from tgbot.models.bet_order import BetOrder
 
 
 class BetOrderService:
     def __init__(self):
-        self.bet_api_client = BetOrderApiClient()
+        self.bet_order_api_client = BetOrderApiClient()
+        self.bet_api_client = BetApiClient()
 
     def build(self, chat_id, user_id, bet_code, bot_id, amount, bet_num=None):
         """
         创建投注单
         :return:
         """
-        bet_order = BetOrder(chat_id, user_id, bet_code, bot_id, amount, bet_num)
-        return self.bet_api_client.build(bet_order)
+        is_bet = self.bet_api_client.check_status(chat_id, bot_id)
+        if is_bet:
+            bet_order = BetOrder(chat_id, user_id, bet_code, bot_id, amount, bet_num)
+            return self.bet_order_api_client.build(bet_order)
+        else:
+            return False, "封盘中禁止投注"
 
     def get(self, chat_id, user_id, bot_id):
         """
@@ -22,4 +28,4 @@ class BetOrderService:
         :param bot_id:机器人id
         :return:详情
         """
-        return self.bet_api_client.get(chat_id, user_id, bot_id)
+        return self.bet_order_api_client.get(chat_id, user_id, bot_id)
